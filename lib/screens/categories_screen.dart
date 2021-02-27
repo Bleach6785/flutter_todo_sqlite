79cb83f4499b41a7af3e607d897c051b211cf3f8
Lu-Yi Shi <cloudx007@gmail.com>
2021-02-27 16:37:12 +0800

    Setting Up Layout Categories Screen

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo_sqlite/screens/home_screen.dart';
 
 class CategoriesScreen extends StatefulWidget {
   @override
@@ -6,8 +7,78 @@ class CategoriesScreen extends StatefulWidget {
 }
 
 class _CategoriesScreenState extends State<CategoriesScreen> {
+  _showFormDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (param) {
+        return AlertDialog(
+          actions: [
+            FlatButton(
+              color: Colors.red,
+              onPressed: () => Navigator.pop(context),
+              child: Text('Canel'),
+            ),
+            FlatButton(
+              color: Colors.blue,
+              onPressed: () {},
+              child: Text('Save'),
+            ),
+          ],
+          title: Text('Categories Form'),
+          content: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Write a category',
+                    labelText: 'Category',
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Write a description',
+                    labelText: 'Description',
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Welcome to Categories Screen');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Categories'),
+        leading: RaisedButton(
+          onPressed: () => Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => HomeScreen(),
+            ),
+          ),
+          child: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          color: Colors.blue,
+          elevation: 0.0, // 去除陰影
+        ),
+      ),
+      body: Center(
+        child: Text('Welcome to Categories Screen'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showFormDialog(context);
+        },
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+    );
   }
 }
