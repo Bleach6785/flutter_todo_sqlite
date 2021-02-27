1db6f0a33a2e9bd58151fbc59199cfa53a6904d9
Lu-Yi Shi <cloudx007@gmail.com>
2021-02-27 13:02:16 +0800

    Setting Up Layout Drawer Navigation

--- lib/helpers/drawer_navigation.dart
+++ lib/helpers/drawer_navigation.dart
@@ -12,6 +12,40 @@ class _DrawerNavigationState extends State<DrawerNavigation> {
   Widget build(BuildContext context) {
     return Container(
       child: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://pic.pimg.tw/hanzhiyu/1605433233-1520719975-g_m.jpg'),
+              ),
+              accountName: Text('User Name'),
+              accountEmail: Text('xxxx@xxx.xxx'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              onTap: () => Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => HomeScreen(),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.view_list),
+              title: Text('Categories'),
+              onTap: () => Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => CategoriesScreen(),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
           child: ListView(
         children: [
           UserAccountsDrawerHeader(
