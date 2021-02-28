30ede0f7353491ab9eb3922f32031de421603230
Lu-Yi Shi <cloudx007@gmail.com>
2021-02-28 15:21:57 +0800

    Setting Up Layout Create Todo Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todo_sqlite/helpers/drawer_navigation.dart';
+import 'package:flutter_todo_sqlite/screens/todo_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -14,6 +15,14 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text('Todolist Sqlite'),
       ),
       drawer: DrawerNavigation(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => TodoScreen(),
+          ),
+        ),
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
