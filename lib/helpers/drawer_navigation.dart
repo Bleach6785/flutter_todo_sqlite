c706474e39034bab9288c2cf8e38f4a5a1f8447a
Lu-Yi Shi <cloudx007@gmail.com>
2021-02-28 20:14:13 +0800

    Show All Categories Item on Drawer Navigation

--- lib/helpers/drawer_navigation.dart
+++ lib/helpers/drawer_navigation.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todo_sqlite/screens/categories_screen.dart';
 import 'package:flutter_todo_sqlite/screens/home_screen.dart';
+import 'package:flutter_todo_sqlite/services/category_service.dart';
 
 class DrawerNavigation extends StatefulWidget {
   @override
@@ -8,6 +9,28 @@ class DrawerNavigation extends StatefulWidget {
 }
 
 class _DrawerNavigationState extends State<DrawerNavigation> {
+  List<Widget> _categoryList = List<Widget>();
+
+  CategoryService _categoryService = CategoryService();
+
+  @override
+  void initState() {
+    super.initState();
+    getAllCategories();
+  }
+
+  getAllCategories() async {
+    var categories = await _categoryService.readCategories();
+
+    categories.forEach((category) {
+      setState(() {
+        _categoryList.add(ListTile(
+          title: Text(category['name']),
+        ));
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -42,6 +65,10 @@ class _DrawerNavigationState extends State<DrawerNavigation> {
                   builder: (context) => CategoriesScreen(),
                 ),
               ),
+            ),
+            Divider(),
+            Column(
+              children: _categoryList,
             ),
           ],
         ),
